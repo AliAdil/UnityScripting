1f24f9c88b358a04d2e93f9b7282ec292dd85ec4
aliadil <aliadilg@gmail.com>
2018-01-02 17:32:12 +0500

    Array Script

---
+++ Assets/Scripts/ArrayList.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrayList : MonoBehaviour {
+	public string[] members = new string[] {"ALi","Adil","Usman","Bilal"};
+	// Use this for initialization
+
+
+	void Start () {
+		Debug.Log (members [1]+" "+members[0]);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
