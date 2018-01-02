3ded8c99df46c3059e93076ae6a9fec2493206e2
aliadil <aliadilg@gmail.com>
2018-01-03 02:00:31 +0500

    LoopsInStatments

---
+++ Assets/Scripts/LoopsInStatements.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopsInStatements : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i <= 100; i++)
+        {
+            if (i == 0)
+            {
+                Debug.Log(i + "number is Zero");
+            }
+            else if (IsEven(i))
+            {
+                Debug.Log(i + "is even");
+            }
+            else
+            {
+                Debug.Log(i + "is odd");
+            }
+        }
+		
+	}
+
+    public bool IsEven(int numb)
+    {
+        if (numb % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
