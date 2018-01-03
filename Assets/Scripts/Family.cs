d862f526e8790d18905aebe027da92e728bd8702
aliadil <aliadilg@gmail.com>
2018-01-03 17:18:08 +0500

    Classes and Object

---
+++ Assets/Scripts/Family.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Family : MonoBehaviour {
+
+    public Person father;
+    public Person mother;
+    public Person son;
+
+	// Use this for initialization
+	void Start () {
+        father = new Person();
+        father.firstName = "Ali";
+        father.lastName = "Adil";
+        father.age = 26;
+        father.isMale = true;
+        father.isMarried = true;
+
+        mother = new Person();
+        mother.firstName = "Xyx";
+        mother.lastName = "Sweet";
+        mother.age = 24;
+        mother.isMale = false;
+        mother.isMarried = true;
+
+        son = new Person();
+        son.firstName = "Abc";
+        son.lastName = "cba";
+        son.age = 3;
+        son.isMale = true;
+        son.isMarried = false;
+
+
+        Debug.Log(father.firstName + " and "+ mother.firstName+" have beautiful son "+son.firstName);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person  {
+
+    public string firstName = "";
+    public string lastName = "";
+    public int age = 0;
+    public bool isMale = false;
+    public bool isMarried = false;
+	
+}
