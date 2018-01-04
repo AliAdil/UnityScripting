7587a3b96837e13efa398946702f46598ab2aeb3
aliadil <aliadilg@gmail.com>
2018-01-04 22:58:33 +0500

    Person class update

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -6,8 +6,32 @@ public class Person  {
 
     public string firstName = "";
     public string lastName = "";
+    public Person spouse;
+    // below varibale are used in family class 
     public int age = 0;
     public bool isMale = false;
     public bool isMarried = false;
+
+    public bool IsMarriedWith(Person otherPerson)
+    {
+        if (spouse != null)
+        {
+            // person object is stored in spouse variable 
+            if (otherPerson == this.spouse)
+            {
+                //otherPerson object is the same as stored spouse
+                return true;
+            }
+            else
+            {
+                //not married
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
 	
 }
